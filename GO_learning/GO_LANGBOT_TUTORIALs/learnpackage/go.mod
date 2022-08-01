35f3177a17e1c7962a55ca935960b940e5254954
dann-g <dn_ng@tutanota.com>
2022-08-01 10:11:32 -0500

    adding control flow

--- GO_learning/GO_LANGBOT_TUTORIALs/learnpackage/go.mod
+++ GO_learning/GO_LANGBOT_TUTORIALs/learnpackage/go.mod
@@ -1,3 +1,3 @@
 module learnpackage
 
-go 1.18
+go 1.13
--- GO_learning/GO_LANGBOT_TUTORIALs/learnpackage/main.go
+++ GO_learning/GO_LANGBOT_TUTORIALs/learnpackage/main.go
@@ -1,5 +1,26 @@
 package main 
-import "fmt"
+
+import( "fmt"
+  "learnpackage/simpleinterest"
+  "log"
+ )
+ var principal = -5000.0
+ var rate = 10.0
+ var time = 1.0
+func init() {
+fmt.Println("Main package initialized")
+if principal < 0{
+	log.Fatal("Principal is less than zero")
+}
+if rate < 0{
+	log.Fatal("Rate of interest is less than zero")
+}
+if time < 0{
+	log.Fatal("Duration is less than zero")
+ }
+}
 func main(){
-	fmt.Println("Simple Interest Calculation")
+ fmt.Println("Simple Interest Calculation")
+ interest_est:= simpleinterest.Calculate(principal,rate,time)
+ fmt.Println("Simple interest is",interest_est)
 }
